64289c0594d3ad9e97c20fda43b167c1689f82eb
Tyler Oakleaf <tyler@superpedestrian.com>
2020-03-01 20:36:28 -0500

    points are being collected

--- src/PaintArea.svelte
+++ src/PaintArea.svelte
@@ -1,5 +1,40 @@
 <script>
+  let points = [];
+  let dragging = false;
 
+  $: console.log(points);
+
+  const mousePos = event => {
+    const bounds = event.target.getBoundingClientRect();
+    return { x: event.clientX - bounds.left, y: event.clientY - bounds.top };
+  };
+
+  const mouseDown = event => {
+    dragging = true;
+    points = [mousePos(event)];
+  };
+
+  const drag = event => {
+    if (dragging) points = [...points, mousePos(event)];
+  };
+
+  const mouseUp = event => {
+    dragging = false;
+  };
 </script>
 
-<button class="button is-primary">Button</button>
+<style>
+  svg {
+    background: black;
+    min-height: 500px;
+  }
+</style>
+
+<p>Dragging? {dragging}</p>
+<svg
+  height="100%"
+  width="100%"
+  on:mousedown={mouseDown}
+  on:mousemove={drag}
+  on:mouseup={mouseUp}
+  on:mouseleave={mouseUp} />
